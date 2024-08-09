6e6d4950f2d249d2456967afbc42ee6d78c8f5b3
nadeemasgar <nadeemasgar18@gmail.com>
2024-08-10 00:45:30 +0530

    using react via CDN

---
+++ 01. JsCafe React/index.js
@@ -0,0 +1,6 @@
+// const div = React.createElement('div', {className: 'text'}, "hello from vedant, i am inside react now")
+const child1 = React.createElement('p', {}, "I am a child1")
+const child2 = React.createElement('p', {}, "I am child2")
+const div = React.createElement('div', {className: 'text'}, [child1, child2]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(div);
