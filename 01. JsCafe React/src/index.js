12c8195ffcf27d4fa152fec198e3a29cef219ec2
nadeemasgar <nadeemasgar18@gmail.com>
2024-08-10 13:14:36 +0530

    Setup react app via webpack

--- 01. JsCafe React/src/index.js
+++ 01. JsCafe React/src/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 // const div = React.createElement('div', {className: 'text'}, "hello from vedant, i am inside react now")
 const child1 = React.createElement('p', {}, "I am a child1")
 const child2 = React.createElement('p', {}, "I am child2")
